bd0ac66d20f441029ca136f2c938da2d757dea41
Shaloy Lewis <shaloylewis1998@gmail.com>
2024-05-21 23:31:02 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
-
-"""HYPEN_E_DOT='-e .'
+'''
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
@@ -12,13 +12,12 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
-
+    return requirements
+'''
 setup(
-    name='cerdit-card-default-prediction',
+    name='src',
     version='0.0.1',
     author='Shaloy Lewis',
     author_email='shaloylewis1998@gmail.com',
-    install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-from src.logger.logging import logging
-from src.exception.exception import customexception
-
-
 import os
 import sys
+
+from src.logger.logging import logging
+from src.exception.exception import customexception
+from src.utils.constants import DATA_PATH, TARGET_FEATURE
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from pathlib import Path
@@ -13,8 +14,10 @@ from pathlib import Path
 @dataclass
 class DataIngestionConfig:
     raw_data_path:str=os.path.join("artifacts","raw.csv")
-    train_data_path:str=os.path.join("artifacts","train.csv")
-    test_data_path:str=os.path.join("artifacts","test.csv")
+    train_data_path:str=os.path.join("artifacts","X_train.csv")
+    test_data_path:str=os.path.join("artifacts","X_test.csv")
+    train_target_path:str=os.path.join("artifacts","y_train.csv")
+    test_target_path:str=os.path.join("artifacts","y_test.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -24,7 +27,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("initiating data ingestion")
         try:
-            filepath="data\UCI_Credit_Card.csv"
+            filepath=Path(DATA_PATH)
             data=pd.read_csv(filepath)
             logging.info("data read successfully")
 
@@ -34,22 +37,31 @@ class DataIngestion:
             
             logging.info("initiating train test split")
             
-            train_data,test_data=train_test_split(data,test_size=0.3)
+            X=data.drop(columns=TARGET_FEATURE)
+            y=data[TARGET_FEATURE]
+
+            X_train,X_test,y_train,y_test=train_test_split(X
+                                               , y
+                                               , test_size=0.30
+                                               , shuffle=True
+                                               , stratify=y 
+                                               , random_state=42)
+            
             logging.info("train test split completed")
             
-            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
-            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            X_train.to_csv(self.ingestion_config.train_data_path,index=False)
+            X_test.to_csv(self.ingestion_config.test_data_path,index=False)
+            y_train.to_csv(self.ingestion_config.train_target_path,index=False)
+            y_test.to_csv(self.ingestion_config.test_target_path,index=False)
             
-            logging.info("data ingestion part completed")
+            logging.info("data ingestion completed")
             
-            return (
-                 
-                
+            return (             
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.train_target_path,
+                self.ingestion_config.test_target_path
             )
-
-
 
         except Exception as e:
             logging.info()
